15f89f4be6392d9536bf07b325e4442480659b16
Eduardo Gerhardt <edulg101@yahoo.com.br>
2020-06-27 19:42:42 -0300

    Acrescentando comentários aos posts

--- src/main/java/SpringBootMongoDB/config/Instantiation.java
+++ src/main/java/SpringBootMongoDB/config/Instantiation.java
@@ -3,6 +3,7 @@ package SpringBootMongoDB.config;
 import SpringBootMongoDB.domain.Post;
 import SpringBootMongoDB.domain.User;
 import SpringBootMongoDB.dto.AuthorDTO;
+import SpringBootMongoDB.dto.CommentDTO;
 import SpringBootMongoDB.repository.PostRepository;
 import SpringBootMongoDB.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null,sdf.parse("21/03/2018"), "pariu viagem", "Vou viajar para SP", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acorde Feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO com1 = new CommentDTO("Boa Viagem Mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO com2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO com3 = new CommentDTO("Tenha um otimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(com1,com2));
+        post2.getComments().addAll(Arrays.asList(com2));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
